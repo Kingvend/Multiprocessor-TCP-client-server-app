791be696ebb9d33dd094e7ed67c68a92c4f5f914
Kingvend <gil_alex97@mail.ru>
2020-05-08 14:22:05 +0500

    modified file

--- server_3 copy.cpp
+++ server_3 copy.cpp
@@ -23,7 +23,7 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/signal.h> fhgfghfhg
+#include <sys/signal.h>
 #include <sys/wait.h>
 #include <sys/resource.h>
 #include <netinet/in.h>
